c8b66373eeef89368d8b87498cfaa1b4359e15b0
Dennis Conner <dconner@gregoryschool.org>
2024-12-09 08:48:50 -0700

    12/9 One Drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OneDriveJr.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OneDriveJr.java
@@ -12,7 +12,7 @@ import com.qualcomm.robotcore.hardware.Servo;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
-
+//For FLL scrimmage
 //@Disabled
 @TeleOp(name = "YaelDriveJr", group = "YaelDriveJr")
 
