35526ced3f3a21ce7fe188e2ec7201021b4b9330
MarsMan <ebialis@gregoryschool.org>
2025-01-10 14:57:01 -0700

    Pre-Practice FInal Updates

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/DoubleSlideDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/DoubleSlideDrive.java
@@ -118,7 +118,7 @@ public class DoubleSlideDrive extends LinearOpMode {
 
             // Handle D-Pad inputs for shoulder and wrist positions
             if (gamepad1.dpad_down) {
-                setShoulderAndWristPositions(leftShoulder, rightShoulder, wristServo, 0.8, 0.9);
+                setShoulderAndWristPositions(leftShoulder, rightShoulder, wristServo, 0.3, 0.9);
             } else if (gamepad1.dpad_left) {
                 setShoulderAndWristPositions(leftShoulder, rightShoulder, wristServo, 1.0, 0.7);
             } else if (gamepad1.dpad_up) {
